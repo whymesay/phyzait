2f1807b94b19b417ba186ae00b7dbcdbe323bdd6
whymesay <dioojj99@gmail.com>
2022-04-26 15:44:32 +0800

    Initial commit

---
+++ src/main/java/com/github/whymesay/phyzait/phyzait/PhyzaitApplication.java
@@ -0,0 +1,13 @@
+package com.github.whymesay.phyzait.phyzait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhyzaitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhyzaitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/whymesay/phyzait/phyzait/PhyzaitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.whymesay.phyzait.phyzait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhyzaitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
